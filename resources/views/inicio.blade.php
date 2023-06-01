a705141dc866f6dd3936e73e7e83ea24a02e7c3b
Iara Rodrigues Abreu <iara.abreu@tjro.jus.br>
2023-06-01 12:40:14 -0400

    wip

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,53 +1,59 @@
 @extends('layout.default')
 
-@section('content')
-    <header>
-        <nav>
-            <!-- Seu código HTML para a barra de navegação -->
-        </nav>
-    </header>
-
-    <section class="hero">
-        <div class="container">
-            <h1>Bem-vindo ao HELP PVH</h1>
-            <p>A plataforma de colaboração para melhorias da cidade</p>
-            <a href="#" class="btn btn-primary">Comece agora</a>
-        </div>
-    </section>
-
-    <section class="features">
-        <div class="container">
-            <h2>Recursos Principais</h2>
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="feature">
-                        <i class="fas fa-users"></i>
-                        <h3>Colaboração</h3>
-                        <p>Colabore com outros cidadãos para melhorar a sua cidade.</p>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="feature">
-                        <i class="fas fa-map-marked-alt"></i>
-                        <h3>Localização</h3>
-                        <p>Encontre e compartilhe informações sobre melhorias em locais específicos.</p>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="feature">
-                        <i class="fas fa-cogs"></i>
-                        <h3>Gestão</h3>
-                        <p>Acompanhe o progresso das melhorias e colabore com a gestão.</p>
-                    </div>
+@section('main')
+    <div class="container border col-12 bg-primary text-light text-center">
+        <div class="box mt-2">
+            <div class="box-header">
+                <div class="box-title">
+                    <h1>Help PVH</h1>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+    <br>
 
-    <section class="cta">
-        <div class="container">
-            <h2>Junte-se a nós e faça a diferença!</h2>
-            <a href="#" class="btn btn-primary">Cadastre-se agora</a>
+    <div class="box-body text-center">
+        <div class="form-group">
+            <h1>Bem-vindo ao HELP PVH</h1>
+                            <img src="/public/imagem/portovelho.jpeg" alt="imagem">
+{{--                            <a href="resources/img/portovelho.jpeg"></a>--}}
+{{--                            <buttton>Inscreva-se</buttton>--}}
         </div>
-    </section>
+        </div>
+{{--        <div class="container">--}}
+{{--            <p>A plataforma de colaboração para melhorias da cidade</p>--}}
+{{--            <a href="#" class="btn btn-primary">Comece agora</a>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+
+{{--    <div class="box-body">--}}
+{{--        <div class="form-group">--}}
+{{--            <div class="col-md-10">--}}
+{{--                <h2>Recursos Principais:</h2>--}}
+{{--                <br>--}}
+{{--                <h3>Colaboração</h3>--}}
+{{--                <p>Colabore com outros cidadãos para melhorar a sua cidade.</p>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--        <div class="col-md-4">--}}
+{{--            <div class="feature">--}}
+{{--                <i class="fas fa-map-marked-alt"></i>--}}
+{{--                <h3>Localização</h3>--}}
+{{--                <p>Encontre e compartilhe informações sobre melhorias em locais específicos.</p>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--        <div class="col-md-4">--}}
+{{--            <div class="feature">--}}
+{{--                <i class="fas fa-cogs"></i>--}}
+{{--                <h3>Gestão</h3>--}}
+{{--                <p>Acompanhe o progresso das melhorias e colabore com a gestão.</p>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--        <div class="col-md-4">--}}
+{{--            <h2>Junte-se a nós e faça a diferença!</h2>--}}
+{{--            <a href="#" class="btn btn-primary">Cadastre-se agora</a>--}}
+{{--        </div>--}}
+{{--    </div>--}}
 @endsection
+
+
