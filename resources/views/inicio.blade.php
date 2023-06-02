96dbd22b1f46c23fbee6b4c946ba75369dcc9656
Iara Rodrigues Abreu <iara.abreu@tjro.jus.br>
2023-06-02 11:42:51 -0400

    wip

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -10,50 +10,52 @@
             </div>
         </div>
     </div>
-    <br>
-
+    <br><br>
     <div class="box-body text-center">
-        <div class="form-group">
-            <h1>Bem-vindo ao HELP PVH</h1>
-                            <img src="/public/imagem/portovelho.jpeg" alt="imagem">
-{{--                            <a href="resources/img/portovelho.jpeg"></a>--}}
-{{--                            <buttton>Inscreva-se</buttton>--}}
-        </div>
-        </div>
-{{--        <div class="container">--}}
-{{--            <p>A plataforma de colaboração para melhorias da cidade</p>--}}
-{{--            <a href="#" class="btn btn-primary">Comece agora</a>--}}
-{{--        </div>--}}
-{{--    </div>--}}
+        <div class="box-header with-border">
+            <div class="form-group">
+                <h1>Bem-vindo ao HELP PVH</h1>
+                <br>
+                <div class="box-body">
+                    <div class="col-md-10 text-right">
+                        <h2>Recursos Principais:</h2>
+                        {{--                <br>--}}
+                        {{--                <h3>Colaboração</h3>--}}
+                        {{--                <p>Colabore com outros cidadãos para melhorar a sua cidade.</p>--}}
 
-{{--    <div class="box-body">--}}
-{{--        <div class="form-group">--}}
-{{--            <div class="col-md-10">--}}
-{{--                <h2>Recursos Principais:</h2>--}}
-{{--                <br>--}}
-{{--                <h3>Colaboração</h3>--}}
-{{--                <p>Colabore com outros cidadãos para melhorar a sua cidade.</p>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--        <div class="col-md-4">--}}
-{{--            <div class="feature">--}}
-{{--                <i class="fas fa-map-marked-alt"></i>--}}
-{{--                <h3>Localização</h3>--}}
-{{--                <p>Encontre e compartilhe informações sobre melhorias em locais específicos.</p>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--        <div class="col-md-4">--}}
-{{--            <div class="feature">--}}
-{{--                <i class="fas fa-cogs"></i>--}}
-{{--                <h3>Gestão</h3>--}}
-{{--                <p>Acompanhe o progresso das melhorias e colabore com a gestão.</p>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--        <div class="col-md-4">--}}
-{{--            <h2>Junte-se a nós e faça a diferença!</h2>--}}
-{{--            <a href="#" class="btn btn-primary">Cadastre-se agora</a>--}}
-{{--        </div>--}}
-{{--    </div>--}}
+                        <div class="col-md-5">
+                            <img src={{asset('img/mapalocal.png')}} width="700" height="650" alt="imagem"
+                                 style="border: 5px solid #ccc; border-radius: 15px;">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{--        <div class="col-md-4">--}}
+    {{--            <div class="feature">--}}
+    {{--                <i class="fas fa-map-marked-alt"></i>--}}
+    {{--                <h3>Localização</h3>--}}
+    {{--                <p>Encontre e compartilhe informações sobre melhorias em locais específicos.</p>--}}
+    {{--            </div>--}}
+    {{--        </div>--}}
+    {{--        <div class="col-md-4">--}}
+    {{--            <div class="feature">--}}
+    {{--                <i class="fas fa-cogs"></i>--}}
+    {{--                <h3>Gestão</h3>--}}
+    {{--                <p>Acompanhe o progresso das melhorias e colabore com a gestão.</p>--}}
+    {{--            </div>--}}
+    {{--        </div>--}}
+    {{--        <div class="col-md-4">--}}
+    {{--            <h2>Junte-se a nós e faça a diferença!</h2>--}}
+    {{--            <a href="#" class="btn btn-primary">Cadastre-se agora</a>--}}
+    {{--        </div>--}}
+    {{--    </div>--}}
+    {{--        <div class="container">--}}
+    {{--            <p>A plataforma de colaboração para melhorias da cidade</p>--}}
+    {{--            <a href="#" class="btn btn-primary">Comece agora</a>--}}
+    {{--        </div>--}}
+    {{--    </div>--}}
 @endsection
 
 
