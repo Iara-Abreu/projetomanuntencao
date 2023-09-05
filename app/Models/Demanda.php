0d409c7970c4794b5848f34a4f630390ae68bac6
amosoliver <oliveramos13542@gmail.com>
2023-09-05 10:02:54 -0400

    feed temporario

--- app/Models/Demanda.php
+++ app/Models/Demanda.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Demanda extends Model
 {
@@ -11,5 +12,12 @@ class Demanda extends Model
 
     protected $primaryKey = 'id_demanda';
 
-    public
+    public function bairro()
+    {
+        return $this->belongsTo(Bairro::class, 'id_bairro');
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_usuario');
+    }
 }
