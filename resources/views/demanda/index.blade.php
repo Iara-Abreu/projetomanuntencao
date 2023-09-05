a9a29dec575ef05adab50dea27bc277f75d01c60
amosoliver <oliveramos13542@gmail.com>
2023-09-05 17:16:43 -0400

    wip

--- resources/views/demanda/index.blade.php
+++ resources/views/demanda/index.blade.php
@@ -1,75 +1,72 @@
 @extends('layout.default')
 @section('main')
-<div class="container mt-2 pb-5 ">
-    <div class="container-fluid mt-2 ml-3 mr-3">
-        <div class="box">
-            <div class="box-header d-flex justify-content-between align-items-center">
-                <div class="box-title">
-                    <h1>{{ $title }}</h1>
-                </div>
-                <div>
-                    <a href="{{ route('demanda.create') }}" class="btn btn-success">Adicionar</a>
+    <div class="container mt-2 pb-5 ">
+        <div class="container-fluid mt-2 ml-3 mr-3">
+            <div class="box">
+                <div class="box-header d-flex justify-content-between align-items-center">
+                    <div class="box-title">
+                        <h1>{{ $title }}</h1>
+                    </div>
+                    <div>
+                        <a href="{{ route('demanda.create') }}" class="btn btn-success">Adicionar</a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="container-fluid gedf-wrapper">
-        <div class="row">
-            <div class="col-md-3">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="h5">{{ Auth::user()->name }}</div>
-                        <div class="h7 text-muted">Fullname : Miracles Lee Cross</div>
-                        <div class="h7">Developer of web applications, JavaScript, PHP, Java, Python, Ruby, Java,
-                            Node.js,
-                            etc.
+        <div class="container-fluid gedf-wrapper">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="h5">{{ Auth::user()->name }}</div>
+                            <div class="h7 text-muted">Fullname : Miracles Lee Cross</div>
+                            <div class="h7">Developer of web applications, JavaScript, PHP, Java, Python, Ruby, Java,
+                                Node.js,
+                                etc.
+                            </div>
                         </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">
+                                <div class="h6 text-muted">Followers</div>
+                                <div class="h5">5.2342</div>
+                            </li>
+                            <li class="list-group-item">
+                                <div class="h6 text-muted">Following</div>
+                                <div class="h5">6758</div>
+                            </li>
+                            <li class="list-group-item">Vestibulum at eros</li>
+                        </ul>
                     </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">
-                            <div class="h6 text-muted">Followers</div>
-                            <div class="h5">5.2342</div>
-                        </li>
-                        <li class="list-group-item">
-                            <div class="h6 text-muted">Following</div>
-                            <div class="h5">6758</div>
-                        </li>
-                        <li class="list-group-item">Vestibulum at eros</li>
-                    </ul>
                 </div>
-            </div>
-            <div class="col-md-6 gedf-main">
+                <div class="col-md-6 gedf-main">
 
-                @foreach ($demanda as $dem)
-                <div class="mt-3 pb-5">
-                    <div class="card gedf-card">
-                        <div class="card-header">
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="mr-2">
-                                        <img class="rounded-circle" width="45" src="{{$dem->user->url_imagem}}" alt="">
+                    @foreach ($demanda as $dem)
+                        <div class="mt-3 pb-5">
+                            <div class="card gedf-card style="max-width: 100%;">
+                                <div class="card-header">
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <div class="d-flex justify-content-between align-items-center">
+                                            <div class="mr-2">
+                                                <img class="rounded-circle" width="35"
+                                                    src="{{ $dem->user->url_imagem }}" alt="">
+                                            </div>
+                                            <div class="ml-2">
+                                                <div class="h5 m-0">{{ $dem->user->username }}</div>
+                                                <div class="h7 text-muted">{{ $dem->time() }}</div>
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div class="ml-2">
-                                        <div class="h5 m-0">{{ $dem->user->username }}</div>
-                                        <div class="h7 text-muted">{{ $dem->bairro->name }}</div>
+                                    <div class="card-body">
+                                        <p class="card-text">
+                                            {{ $dem->ds_demanda }}
+                                        </p>
+                                        <img src="{{ $dem->url_imagem }}" class="card-img-top" alt="" </div>
+                                    </div>
+                                    <div class="card-footer">
+                                        <a href="#" class="card-link"><i class="fa fa-gittip"></i> Like</a>
+                                        <a href="#" class="card-link"><i class="fa fa-comment"></i> Comment</a>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="card-body">
-                                <div class="text-muted h7 mb-2"> <i class="fa fa-clock-o"></i>
-                                {{$dem->time()}}
-                                <p class="card-text">
-                                    {{$dem->ds_demanda}}
-                                </p>
-                                
-                            </div>
-                            <div class="card-footer">
-                                <a href="#" class="card-link"><i class="fa fa-gittip"></i> Like</a>
-                                <a href="#" class="card-link"><i class="fa fa-comment"></i> Comment</a>
-                            </div>
-                        </div>
-                    </div>
+                    @endforeach
                 </div>
-                @endforeach
-            </div>
-@endsection
+            @endsection
